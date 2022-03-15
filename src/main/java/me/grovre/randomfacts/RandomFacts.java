00fa2c0e51f720c877a93ceaf19ac018a633a5be
Grovre <50428844+Grovre@users.noreply.github.com>
2022-03-15 15:09:37 -0500

    possibly done

--- src/main/java/me/grovre/randomfacts/RandomFacts.java
+++ src/main/java/me/grovre/randomfacts/RandomFacts.java
@@ -1,17 +1,57 @@
 package me.grovre.randomfacts;
 
+import com.mashape.unirest.http.Unirest;
+import me.grovre.randomfacts.events.JokeEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+
 public final class RandomFacts extends JavaPlugin {
+
+    public static RandomFacts plugin;
+
+    public static RandomFacts getPlugin() {
+        return plugin;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
+        saveDefaultConfig();
+        int delay = this.getConfig().getInt("intervalBetweenFacts");
 
+        Runnable joker = () -> {
+            String joke = Unirest.get("https://uselessfacts.jsph.pl/random.txt?language=en")
+                    .toString()
+                    .split("\n")[0]
+                    .substring(1);
+            try {
+                Unirest.shutdown();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for(Player p : Bukkit.getOnlinePlayers()) {
+                JokeEvent event = new JokeEvent(p, joke);
+                Bukkit.getPluginManager().callEvent(event);
+                p.sendMessage(joke);
+            }
+        };
+
+        Bukkit.getScheduler().runTaskTimerAsynchronously(this, joker, 600, delay);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        try {
+            Unirest.shutdown();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
---
+++ src/main/java/me/grovre/randomfacts/events/JokeEvent.java
@@ -0,0 +1,49 @@
+package me.grovre.randomfacts.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class JokeEvent extends Event implements Cancellable {
+
+    private boolean cancelled;
+    private Player player;
+    private String joke;
+    private static final HandlerList handlers = new HandlerList();
+
+    public JokeEvent(Player player,String joke) {
+        cancelled = false;
+        this.player = player;
+        this.joke = joke;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+
+    @Override
+    @NonNull
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        cancelled = cancel;
+    }
+}
